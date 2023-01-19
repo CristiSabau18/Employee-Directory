6e4ba1529fd0cf523ba88a0ef87ff948a60a9f1a
CristiSabau18 <cristisabau18@yahoo.com>
2023-01-19 19:03:50 +0200

    app.js updated

--- app.js
+++ app.js
@@ -49,7 +49,7 @@ function displayModal(index) {
             <p class="address">${city}</p>
             <hr />
             <p>${phone}</p>
-            <p class="address">${street}, ${state} ${postcode}</p>
+            <p class="address">${street.number}, ${street.name}, ${state} ${postcode}</p>
             <p>Birthday:
         ${date.getMonth()}/${date.getDate()}/${date.getFullYear()}</p>
         </div>
